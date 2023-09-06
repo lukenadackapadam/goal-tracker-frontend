32e90c414cf6ca1ad4a58ddd9c8752c838c59174
luke <luke.nadackapadam@gmail.com>
2023-09-06 14:13:30 -0500

    Add logout component

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,11 +1,13 @@
 import { Signup } from "./Signup";
 import { Login } from "./Login";
+import { LogoutLink } from "./LogoutLink";
 
 export function Content() {
   return (
     <div>
       <Signup />
       <Login />
+      <LogoutLink />
     </div>
   );
 }
