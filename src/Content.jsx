bfdb9196ed1907b6bcf210407d28bb1f656a8b55
luke <luke.nadackapadam@gmail.com>
2023-09-06 13:04:14 -0500

    Add signup component

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,10 @@
+import { Signup } from "./Signup";
+
 export function Content() {
   return (
     <div>
       <h1>Hello World!</h1>
+      <Signup />
     </div>
   );
 }
---
+++ src/Signup.jsx
@@ -0,0 +1,52 @@
+import axios from "axios";
+import { useState } from "react";
+
+export function Signup() {
+  const [errors, setErrors] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setErrors([]);
+    const params = new FormData(event.target);
+    axios
+      .post("http://localhost:3000/user.json", params)
+      .then((response) => {
+        console.log(response.data);
+        event.target.result();
+        window.location.href = "/";
+      })
+      .catch((error) => {
+        console.log(error.response.data.errors);
+        setErrors(error.response.data.errors);
+      });
+  };
+
+  return (
+    <div id="signup">
+      <h1>Signup</h1>
+      <ul>
+        {errors.map((error) => (
+          <li key={error}>{error}</li>
+        ))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <div>
+          First Name: <input name="first_name" type="text" />
+        </div>
+        <div>
+          Last Name: <input name="last_name" type="text" />
+        </div>
+        <div>
+          Email: <input name="email" type="email" />
+        </div>
+        <div>
+          Password: <input name="password" type="password" />
+        </div>
+        <div>
+          Password: <input name="password_confirmation" type="password" />
+        </div>
+        <button type="submit">Submit!</button>
+      </form>
+    </div>
+  );
+}
