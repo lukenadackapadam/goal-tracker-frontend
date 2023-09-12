94610ae1ce6448bfb72eb1d65e61e01b7886e47f
luke <luke.nadackapadam@gmail.com>
2023-09-12 16:47:51 -0500

    Add social media buttons

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,19 @@
+import { AwesomeButtonSocial } from "react-awesome-button";
+import AwesomeButtonStyles from "react-awesome-button/src/styles/styles.scss";
+
 export function Footer() {
   return (
     <div id="footer">
+      {/* <p id="copyright">Copyright 2023</p> */}
+      <AwesomeButtonSocial icon="false" cssModule={AwesomeButtonStyles} type="reddit">
+        Reddit
+      </AwesomeButtonSocial>
+      <AwesomeButtonSocial id="link" cssModule={AwesomeButtonStyles} type="linkedin">
+        Linkedin
+      </AwesomeButtonSocial>
+      <AwesomeButtonSocial id="github" cssModule={AwesomeButtonStyles} type="github">
+        Github
+      </AwesomeButtonSocial>
       <p id="copyright">Copyright 2023</p>
     </div>
   );
